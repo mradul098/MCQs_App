2406e5a074fe6aa5dc0d13bf38fec37870307e77
Mayank Choudhary <66920751+Mayank151c@users.noreply.github.com>
2021-09-21 18:13:10 +0530

    some changes for deployment

--- public/js/success.js
+++ public/js/success.js
@@ -2,4 +2,4 @@ var queryString = decodeURIComponent(window.location.search); //parsing
 queryString = queryString.substring(1); 
 
 document.getElementById("score-text1").textContent="Test id = "+queryString;
-document.getElementById("score-text2").textContent="Test link= http://localhost:3000/teststart.html?testid="+queryString;
+document.getElementById("score-text2").textContent="Test link= https://mcqs-app.herokuapp.com/teststart.html?testid="+queryString;
