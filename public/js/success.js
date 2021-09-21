59cac1875e28a0db4055b7272a00c1a852264abe
Mayank Choudhary <66920751+Mayank151c@users.noreply.github.com>
2021-09-21 19:17:24 +0530

    Adding changes for online link.

--- public/js/success.js
+++ public/js/success.js
@@ -1,5 +1,6 @@
 var queryString = decodeURIComponent(window.location.search); //parsing 
 queryString = queryString.substring(1);
 
-document.getElementById("score-text1").textContent="Test id = "+queryString;
-document.getElementById("score-text2").innerHTML=`<a href="https://mcqs-app.herokuapp.com/teststart.html?testid=${queryString}">Direct Link</a>`;
+document.getElementById("score-text1").textContent="Test ID = "+queryString;
+document.getElementById("heading").innerHTML=`<p style="font-size:2rem;margin:2rem;font-weight:600;">Test created Successfully</p>`;
+document.getElementById("link").innerHTML=`<a style="font-size:1.7rem;margin-top:2rem;color:blue;" href="https://mcqs-app.herokuapp.com/teststart.html?testid=${queryString}">Direct Link</a>`;
